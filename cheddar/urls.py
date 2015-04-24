aaa468b458b0ec73d4c175993674835c9bdd0df8
Thierry Carrez <thierry@openstack.org>
2015-04-24 15:11:11 +0200

    Support spaces in track names
    Fix URLs so that spaces in track names do not create fail.

--- cheddar/urls.py
+++ cheddar/urls.py
@@ -21,8 +21,8 @@ urlpatterns = patterns(
     '',
     url(r'^$', views.index, name='index'),
     url(r'^(\d+)$', views.trackindex),
-    url(r'^(\d+)/edit/(\S+)$', views.modifysession),
-    url(r'^(\d+)/(\S+)$', views.editsession),
+    url(r'^(\d+)/edit/(.+)$', views.modifysession),
+    url(r'^(\d+)/(.+)$', views.editsession),
     url(r'^logout$', views.dologout),
     url(r'^loggedout$', views.loggedout),
 )
